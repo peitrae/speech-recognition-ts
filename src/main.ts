e873921672561ed585d20c81fdba3e52852c8bda
Peitra Erdi <peitrae@gmail.com>
2023-07-07 23:04:39 +0700

    logic: integrate record button with SpeechRecognition WebAPI

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,42 @@
 import './styles.sass';
 
+
+function toggleRecord() {
+	if (recordBtn.style['animationName'] === 'flash') {
+		recognition.stop();
+
+		recordBtn.style['animationName'] = 'none';
+		content.innerText = '';
+	} else {
+		recognition.start();
+
+		recordBtn.style['animationName'] = 'flash';
+	}
+}
+
+function initSpeechRecognition(onResult: (e: SpeechRecognitionEvent) => void) {
+	const recognition = new window.SpeechRecognition || new  window.webkitSpeechRecognition;
+
+	recognition.continuous = true;
+	recognition.interimResults = true;
+
+	recognition.onresult = onResult;
+
+	return recognition;
+}
+
+function showTranscriptResult(event: SpeechRecognitionEvent) {
+	let result = '';
+
+	for (let i = event.resultIndex; i < event.results.length; i++) {
+		result += event.results[i][0].transcript;
+	}
+
+	content.innerText = result;
+}
+
+const content = document.querySelector<HTMLDivElement>('#content')!;
+const recordBtn = document.querySelector<HTMLButtonElement>('#record')!;
+recordBtn.addEventListener('click', toggleRecord);
+
+const recognition = initSpeechRecognition(showTranscriptResult);
