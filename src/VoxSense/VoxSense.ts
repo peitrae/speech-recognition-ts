dc553f3d45b596d17cc4015fd5265b0d136e4fa7
Peitra Erdi <peitrae@gmail.com>
2023-07-08 11:00:33 +0700

    logic: Show error if browser does not support SpeechRecognition

--- src/VoxSense/VoxSense.ts
+++ src/VoxSense/VoxSense.ts
@@ -7,45 +7,48 @@ export class VoxSense {
 		this.initialize();
 	}
 
-  /**
-   * Initialize SpeechRecognition. 
-   */
+	/**
+	 * Initialize SpeechRecognition.
+	 */
 	private initialize() {
 		const SpeechRecognition =
 			window.SpeechRecognition || window.webkitSpeechRecognition;
 		// TODO: Add Mozilla, Safari, and Edge compatibility
 
-		// TODO: Handle if browser is unsupported, permission is blocked, and permission is denied.
+		if (!SpeechRecognition) {
+			throw new Error('This browser does not support SpeechRecognition');
+		}
+
+		// TODO: Handle if permission is blocked or denied.
 		this.recognition = new SpeechRecognition();
 	}
 
-  /**
-   * Set recognition options.
-   * 
-   * @param options 
-   */
+	/**
+	 * Set recognition options.
+	 *
+	 * @param options
+	 */
 	private setOptions(options: VoxSenseOptions) {
 		this.recognition.continuous = options.continuous ?? true;
 		this.recognition.interimResults = options.interimResults ?? true;
 		this.recognition.onresult = options.onContinuousResult ?? null;
 	}
 
-  /**
-   * 
-   * Starts the speech recognition service listening to incoming audio.
-   * 
-   * @param options 
-   */
+	/**
+	 *
+	 * Starts the speech recognition service listening to incoming audio.
+	 *
+	 * @param options
+	 */
 	start(options?: VoxSenseOptions) {
 		if (options) this.setOptions(options);
 
 		this.recognition.start();
 	}
 
-
-  /**
-   * Stops the speech recognition service from listening to incoming audio.
-   */
+	/**
+	 * Stops the speech recognition service from listening to incoming audio.
+	 */
 	stop() {
 		this.recognition.stop();
 	}
